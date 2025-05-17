42d1b21969b21169e4945aaedc59f5a36d24f21a
Maykel-Poveda-Paniagua <201285443+Maykel-Poveda-Paniagua@users.noreply.github.com>
2025-05-16 22:03:47 -0600

    Maykel
    Ventana base del Juego

--- Proyecto Tetris/Untitled-1.py
+++ Proyecto Tetris/Untitled-1.py
@@ -1,1 +1,20 @@
-import _tkinter as tk 
+import tkinter as tk
+import random
+from PIL import Image, ImageTk
+
+# Configuracion del juego
+FONDO_DEL_TABLERO = "black"
+
+ANCHO = 12
+ALTURA = 22
+TAMANO_CELDA = 30
+
+bienvenida = tk.Tk()
+bienvenida.geometry("400x600")
+bienvenida.resizable(False,False)
+imagen = Image.open("R.ico")
+icono = ImageTk.PhotoImage(imagen)
+bienvenida.iconphoto(True,icono)
+
+
+bienvenida.mainloop()
